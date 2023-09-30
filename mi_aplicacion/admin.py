d2bfee5bc24dbf25783ac9eae1311c486252fb58
xjuanfran <juan.garces.leon@correunivalle.edu.co>
2023-09-29 19:42:08 -0500

    tables

--- mi_aplicacion/admin.py
+++ mi_aplicacion/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Univalluno
+from .models import ArticuloDeportivo
+from .models import Prestamo
+
 
 # Register your models here.
+
+admin.site.register(Univalluno)
+admin.site.register(ArticuloDeportivo)
+admin.site.register(Prestamo)
--- mi_aplicacion/models.py
+++ mi_aplicacion/models.py
@@ -1,10 +1,30 @@
 # Create your models here.
 from django.db import models
 
-class MiModelo(models.Model):
-    campo1 = models.CharField(max_length=100)
-    campo2 = models.IntegerField()
+class Univalluno(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    tipoUnivalluno = models.CharField(max_length=100)
+    tipoDocumento = models.CharField(max_length=100)
+    numeroDocumento = models.CharField(max_length=10)
+    codigoEstudiante = models.CharField(max_length=10)
+    correoElectrónico = models.CharField(max_length=100)
+
+class ArticuloDeportivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    deporte = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=100)
+
+class Prestamo(models.Model):
+    univalluno = models.ForeignKey(Univalluno, on_delete=models.CASCADE)
+    articulo_deportivo = models.ForeignKey(ArticuloDeportivo, on_delete=models.CASCADE)
+    fecha_prestamo = models.DateField()
+    fecha_devolucion = models.DateField()
 
     def __str__(self):
-        return self.campo1
+        return f"Prestamo de {self.univalluno.nombres} {self.univalluno.apellidos} - {self.articulo_deportivo.nombre}"
+
+
+    
+
 
